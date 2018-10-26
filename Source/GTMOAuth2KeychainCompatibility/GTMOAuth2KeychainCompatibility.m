85b191a23058cf4888417ee6a9dd98c06671cdcb
Anthony Z <z@belua.com>
2018-10-26 17:20:24 +0100

    Using latest AppAuth

--- Source/GTMOAuth2KeychainCompatibility/GTMOAuth2KeychainCompatibility.m
+++ Source/GTMOAuth2KeychainCompatibility/GTMOAuth2KeychainCompatibility.m
@@ -18,12 +18,7 @@
 
 #import "GTMOAuth2KeychainCompatibility.h"
 
-#ifndef GTMAPPAUTH_USER_IMPORTS
-#import <AppAuth/AppAuth.h>
-#else // GTMAPPAUTH_USER_IMPORTS
 #import "AppAuth.h"
-#endif // GTMAPPAUTH_USER_IMPORTS
-
 #import "GTMKeychain.h"
 
 // standard OAuth keys
@@ -121,6 +116,7 @@ static NSString *const kGTMOAuth2ServiceProviderGoogle = @"Google";
                                                  redirectURL:redirectURI
                                                 responseType:OIDResponseTypeCode
                                                        state:nil
+                                                       nonce:nil
                                                 codeVerifier:nil
                                                codeChallenge:nil
                                          codeChallengeMethod:nil
