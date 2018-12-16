5e7bfada9051b2c069d903598141e9604549fc2d
Anthony Z <z@belua.com>
2018-12-16 19:06:09 +0000

    Using Carthage

---
+++ GTMAppAuth/GTMAppAuth.h
@@ -0,0 +1,19 @@
+//
+//  GTMAppAuth.h
+//  GTMAppAuth
+//
+//  Created by Anthony on 16/12/2018.
+//  Copyright © 2018 Felicitas Publica Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTMAppAuth.
+FOUNDATION_EXPORT double GTMAppAuthVersionNumber;
+
+//! Project version string for GTMAppAuth.
+FOUNDATION_EXPORT const unsigned char GTMAppAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMAppAuth/PublicHeader.h>
+
+
